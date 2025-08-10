e4817d110d275e94951cb98f6435c98c6e56c5b2
Vignesh Varanasi <varanasivsv@gmail.com>
2025-08-10 15:34:34 +0530

    feat: add GitHub integration for repository creation and publishing
    Co-authored-by: Pavan Kumar Chennupati <pavankc005@gmail.com>

--- src/routes/craftBench.ts
+++ src/routes/craftBench.ts
@@ -1,111 +1,178 @@
-import { Router, json, Request, Response } from "express";
-import authCheck from "../middlewares/authCheck.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import CraftBench from "../models/CraftBench.js";
-import User from "../models/User.js";
-import { templates } from "../folios/index.js";
+import { Router, json, Request, Response } from 'express'
+import authCheck from '../middlewares/authCheck.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
+import CraftBench from '../models/CraftBench.js'
+import User from '../models/User.js'
+import { templates } from '../folios/index.js'
+import { Octokit } from 'octokit'
+import {
+  commitFolioToGithub,
+  createGithubRepo,
+  publishFolioToGithub
+} from '../utils/github.js'
 
-const craftBench = Router();
-craftBench.use(json());
-craftBench.use(asyncHandler(authCheck));
+const craftBench = Router()
+craftBench.use(json())
+craftBench.use(asyncHandler(authCheck))
 
 craftBench.post(
-  "/new",
+  '/new',
   asyncHandler(async (req: Request, res: Response) => {
     try {
-      const { folioConfig, meta } = req.body;
+      const { folioConfig, meta } = req.body
       if (!folioConfig || !meta) {
         return res
           .status(400)
-          .json({ error: true, message: "Invalid request body" });
+          .json({ error: true, message: 'Invalid request body' })
       }
 
       const newCraftBench = await CraftBench.create({
         craftName: meta.craftName,
         currentConfig: folioConfig,
         userCreated: req.user?.dbData._id,
-        repoLink: "",
-        folioSelected: meta?.folioId,
-      });
+        repoLink: '',
+        folioSelected: meta?.folioId
+      })
 
       if (!newCraftBench) {
         return res
           .status(500)
-          .json({ error: true, message: "Failed to create CraftBench" });
+          .json({ error: true, message: 'Failed to create CraftBench' })
       }
 
       const updatedUser = await User.findByIdAndUpdate(
         {
-          _id: req.user?.dbData._id,
+          _id: req.user?.dbData._id
         },
         {
           $push: {
-            craftBenches: newCraftBench._id,
+            craftBenches: newCraftBench._id
           },
-          recentConfig: folioConfig,
+          recentConfig: folioConfig
         },
         { new: true }
-      );
+      )
 
       if (!updatedUser) {
         return res
           .status(500)
-          .json({ error: true, message: "Failed to update user" });
+          .json({ error: true, message: 'Failed to update user' })
       }
       return res.status(201).json({
         error: false,
-        message: "CraftBench created successfully",
-        craftId: newCraftBench._id.toString(),
-      });
+        message: 'CraftBench created successfully',
+        craftId: newCraftBench._id.toString()
+      })
     } catch (err: any) {
       res.status(500).json({
         error: true,
-        message: "Server Error",
-        errorMessage: err && err.message ? err.message : String(err),
-      });
+        message: 'Server Error',
+        errorMessage: err && err.message ? err.message : String(err)
+      })
     }
   })
-);
+)
 
+craftBench.get(
+  '/download/:craftId',
+  asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const craftId = req.params.craftId as string
+      // console.log('craftId: ', craftId);
 
-craftBench.get('/download/:craftId',asyncHandler(async (req: Request, res: Response) => {
-  try {
-    const craftId = req.params.craftId as string;
-    // console.log('craftId: ', craftId);
-    
-    if (!craftId) {
-      return res.status(400).json({ error: true, message: "Invalid craftId" });
-    }
-
-    const cb  = await CraftBench.findOne(
-      { _id: craftId }
-    ).populate(
-      {
-        path: "folioSelected",
-        select: "folioName",
-        model: "Folio"
+      if (!craftId) {
+        return res.status(400).json({ error: true, message: 'Invalid craftId' })
       }
-    );
-    // console.log('cb: ', cb);
+
+      const cb = await CraftBench.findOne({ _id: craftId }).populate({
+        path: 'folioSelected',
+        select: 'folioName',
+        model: 'Folio'
+      })
+      // console.log('cb: ', cb);
+
+      if (!cb) {
+        return res
+          .status(404)
+          .json({ error: true, message: 'CraftBench not found' })
+      }
+      const folio = cb.folioSelected as { folioName?: string }
+
+      if (!folio || !folio.folioName) {
+        return res.status(404).json({ error: true, message: 'Folio not found' })
+      }
+      const template = templates[folio.folioName]
+      const generatedHTML = template ? template(cb.currentConfig) : ''
+      return res.status(200).send(generatedHTML)
+    } catch (err: any) {
+      res.status(500).json({
+        error: true,
+        message: 'Server Error',
+        errorMessage: err && err.message ? err.message : String(err)
+      })
+    }
+  })
+)
+
+craftBench.post(
+  '/publish/:craftId',
+  asyncHandler(async (req: Request, res: Response) => {
+    const { repoName } = req.body
+
+    // Get CraftName
+    // Get HTMLContent
+
     
-    if (!cb) {
-      return res.status(404).json({ error: true, message: "CraftBench not found" });
+    if (req.user === undefined || !req.user.accessToken) {
+      return res.status(401).json({ error: true, message: 'Unauthorized' })
     }
-    const folio = cb.folioSelected as { folioName?: string };
+    const accessToken = req.user.accessToken
 
-    if (!folio || !folio.folioName) {
-      return res.status(404).json({ error: true, message: "Folio not found" });
+    const octokit = new Octokit({
+      auth: accessToken
+    })
+
+
+    try {
+      const repoResponse = await createGithubRepo(octokit, repoName);
+      
+      const commitResponse = await commitFolioToGithub(
+        octokit,
+        repoName,
+        repoResponse,
+        'Hello'
+      )
+
+      const publishResponse = await publishFolioToGithub(
+        octokit,
+        repoName,
+        repoResponse
+      )
+
+      // to do change status in DB
+
+      res.json({
+        success: true,
+        message:
+          'Successfully Published your Folio',
+        folioUrl: publishResponse
+      })
+    } catch (error: any) {
+      console.error(
+        'Error creating repository, README.md, or preview branch:',
+        error
+      )
+
+      // Handle GitHub API error responses
+      if (error.response) {
+        return res.status(error.response.status).json({
+          error: error.response.data.message || 'Error from GitHub API'
+        })
+      }
+
+      return res.status(500).json({ error: 'Internal Server Error' })
     }
-    const template = templates[folio.folioName];
-    const generatedHTML = template ? template(cb.currentConfig) : "";
-    return res.status(200).send(generatedHTML);
-  } catch (err: any) {
-    res.status(500).json({
-      error: true,
-      message: "Server Error",
-      errorMessage: err && err.message ? err.message : String(err),
-    });
-  }
-}));
+  })
+)
 
-export default craftBench;
+export default craftBench
---
+++ src/utils/github.ts
@@ -0,0 +1,74 @@
+import { Octokit } from 'octokit'
+// Function to create a GitHub repository, add a README.md
+export const createGithubRepo = async (octokit: Octokit, repoName: string) => {
+  // Step 1: Create the repository
+  const repoResponse = await octokit.request('POST /user/repos', {
+    name: repoName,
+    private: false,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+
+  // Step 2: Create the README.md file
+  const readmeContent = `# ${repoName}\n`
+  const contentBase64 = Buffer.from(readmeContent).toString('base64')
+
+  // Create the README.md file in the repository
+  const createReadmeResponse = await octokit.request(
+    'PUT /repos/{owner}/{repo}/contents/{path}',
+    {
+      owner: repoResponse.data.owner.login,
+      repo: repoName,
+      path: 'README.md',
+      message: 'Create default README.md file',
+      content: contentBase64,
+      branch: repoResponse.data.default_branch // Commit on the default branch (main or master)
+    }
+  )
+
+  return repoResponse.data
+}
+
+// Function to get create inde.html and commit it to the GitHub repository
+export const commitFolioToGithub = async (
+  octokit: Octokit,
+  repoName: string,
+  repoResponse: any,
+  HTMLContent: string
+) => {
+  // Step 3: Create index.html in the repo
+  const indexHtmlContent = HTMLContent
+  const indexContentBase64 = Buffer.from(indexHtmlContent).toString('base64')
+
+  // Create the index.html file in the repository
+  const createIndexResponse = await octokit.request(
+    'PUT /repos/{owner}/{repo}/contents/{path}',
+    {
+      owner: repoResponse.owner.login,
+      repo: repoName,
+      path: 'index.html',
+      message: 'Create default index.html file',
+      content: indexContentBase64,
+      branch: repoResponse.default_branch // Commit on the default branch
+    }
+  )
+}
+
+export const publishFolioToGithub = async (
+  octokit: Octokit,
+  repoName: string,
+  repoResponse: any
+) => {
+  // Step 4: Enable GitHub Pages
+  const enablePagesResponse = await octokit.request(
+    'POST /repos/{owner}/{repo}/pages',
+    {
+      owner: repoResponse.owner.login,
+      repo: repoName,
+      source: { branch: 'main', path: '/' }
+    }
+  )
+
+  return enablePagesResponse.data.html_url
+}
